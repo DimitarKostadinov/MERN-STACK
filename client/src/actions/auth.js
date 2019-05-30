a6d7c2ab4b31e6f0b0954f86bb11e977258ba9e8
DimitarKostadinov <mitkokoz@gmail.com>
2019-05-30 12:56:35 +0300

    Add Dashboard functionalities *Add Loading component - Spinner *Add/Edit/Delete Profile *Add/Edit/Delete Experience *Add/Edit/Delete Education *Delete Account with user's posts

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,7 +7,8 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  LOGOUT
+  LOGOUT,
+  CLEAR_PROFILE
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -94,5 +95,6 @@ export const login = (email, password) => async dispatch => {
 };
 // LOGOUT/CLEAR Profile
 export const logout = () => dispach => {
+  dispach({ type: CLEAR_PROFILE });
   dispach({ type: LOGOUT });
 };
